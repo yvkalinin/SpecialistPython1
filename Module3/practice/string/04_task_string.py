52dbc9f6c78cb59d16439a331c433f7ec9390539
boo-learn <t89236238012@gmail.com>
2020-12-05 22:52:00 +0300

    add string practice tasks

---
+++ Module3/practice/string/04_task_string.py
@@ -0,0 +1,7 @@
+# Дана строка текста, слова разделены пробелами, знаки препинания отсутствуют.
+# Определить в предоставленном сообщении количество слов длиной больше, чем 5.
+
+text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
+# Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
+
+# TODO: your code here
---
+++ Module3/practice/string/04a_task_string.py
@@ -0,0 +1,10 @@
+# Дана строка текста, слова разделены пробелами.
+# В предложении могут присутствовать следующие знаки препинания ",.!?-". Знаки препинания частьюслова не являются.
+# Определить в предоставленном сообщении количество слов длиной больше, чем 7.
+
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat consequat vestibulum. " \
+       "Donec tincidunt sed lorem et feugiat. Nullam elementum"
+# Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
+# Примечание: обратите внимание на перенос длинной строки на новую строку
+
+# TODO: your code here
