b8c25d926916abb24e22e0ce35170cb74814fc26
boo-learn <t89236238012@gmail.com>
2020-12-25 14:47:58 +0300

    add tasks for Module-6

---
+++ Module6/home_work/01_hw_file.py
@@ -0,0 +1,8 @@
+# Напишите функцию log() принимающую в качестве аргумента строку и дописывающую это строку в конец файла
+
+def log(text, file="log.txt"):
+    pass
+
+
+log("hello world")  # дописывает "hello world" в конец файла log.txt
+log("message", "log01.txt")  # дописывает "message" в конец файла log01.txt
