879ab12ab7ac3ee8feb39a328aaa324c2af3b56d
yvkalinin <82385156+yvkalinin@users.noreply.github.com>
2021-04-14 15:39:55 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -5,3 +5,11 @@
 def gen_list(size, of, to):
     pass
 
+import random
+def gen_list(size = 10, of = -10, to = 10):
+    new_list = []
+    for _ in range(size):
+        new_list.append(random.randint(of, to))
+    return new_list
+
+print(gen_list())
